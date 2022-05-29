a580d0af92d6099b96b82d597f3b3ce4e37b2f07
unknown <daumminhhuyen275@gmail.com>
2022-05-29 21:49:38 +0700

    first

---
+++ BaiThucHanh2504/src/main/model/animal/tiger.java
@@ -0,0 +1,23 @@
+package model.animal;
+
+public class tiger extends animal{
+    void Eat(){
+        System.out.println("Eat meat");
+    };
+
+    void Sleep(){
+        System.out.println("Sleep");
+    };
+
+    void Walk(){
+        System.out.println("Walk");
+    };
+
+    void Run(){
+        System.out.println("Run");
+    };
+
+    void Roar(){
+        System.out.println("Roar");
+    };
+}
